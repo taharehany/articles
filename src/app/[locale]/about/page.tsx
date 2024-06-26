3be409c2ee7e3a53da08d6d41d5fafbcdb4b7696
Taha <ryhanytaha@gmail.com>
2024-06-27 01:16:02 +0300

    create hero and about

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,5 +1,41 @@
-function AboutPage() {
-	return <div>AboutPage</div>;
-}
+import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import AboutImage from "../../../../public/about.jpg";
+
+const AboutPage = async () => {
+	return (
+		<>
+			<div className='container mx-auto py-10'>
+				<div className='grid grid-cols-1 gap-4 text-center'>
+					<Card>
+						<CardHeader>
+							<div className='mb-6'>
+								<Image src={AboutImage} alt='About Image' className='rounded-full m-auto mb-6' />
+								<Image src={"https://picsum.photos/1500"} alt='About Image' className='rounded-full m-auto mb-6' width={300} height={200} />
+								<Image src={"https://picsum.photos/1500"} alt='About Image' className='rounded-full m-auto mb-6' width={200} height={200} />
+								<Image src={"https://picsum.photos/1500"} alt='About Image' className='rounded-full m-auto mb-6' width={100} height={100} />
+							</div>
+
+							<CardTitle className='text-4xl'>About Us</CardTitle>
+
+							<CardDescription>
+								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias quisquam laudantium architecto? Eligendi adipisci consectetur vitae, culpa veniam recusandae accusantium perferendis velit doloribus ex unde nulla laborum
+								ducimus labore consequatur.
+							</CardDescription>
+						</CardHeader>
+
+						<CardFooter className='flex justify-center'>
+							<Button asChild className='mx-4' variant={"outline"}>
+								<Link href='/'>Go Home</Link>
+							</Button>
+						</CardFooter>
+					</Card>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default AboutPage;
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,27 @@
-import { useTranslations } from "next-intl";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function Index() {
-	const t = useTranslations("Index");
-	return <></>;
-}
+const Index = () => {
+	return (
+		<>
+			<div className='container mx-auto text-center py-16'>
+				<div className='mb-4 h-80 relative rounded-full m-auto overflow-hidden'>
+					<Image src={"https://picsum.photos/2500/1000"} alt='Hero Image' className='w-full object-cover object-center absolute' width={600} height={250} />
+				</div>
+
+				<h1 className='text-4xl mb-4'>Home Page</h1>
+				<p className='text-xl mb-4'>
+					This is the home page of the app Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex explicabo possimus corporis quos? Ad error debitis maxime distinctio odio et, minima quasi, corporis quo assumenda laboriosam voluptate,
+					repellendus atque perspiciatis!
+				</p>
+
+				<Button asChild className='mx-4' variant={"outline"}>
+					<Link href='/about'>Go to About</Link>
+				</Button>
+			</div>
+		</>
+	);
+};
+
+export default Index;
