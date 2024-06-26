b9a50ed0f2a1f4a3f7851a523f8425f9f48c1b61
Taha <ryhanytaha@gmail.com>
2024-06-27 01:15:32 +0300

    create hero and about page

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: ["cdn.sanity.io", "picsum.photos", "images.unsplash.com", "google.com"],
+	},
+};
 
 export default withNextIntl(nextConfig);
